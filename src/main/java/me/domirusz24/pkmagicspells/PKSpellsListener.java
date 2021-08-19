050dfcff7d6619bb79233587f49a596c8f7feaf6
DomiRusz24 <DomiRusz24@gmail.com>
2021-08-19 20:23:20 +0200

    Listener fix, it wasn't checking if BendingPlayer is null.

--- src/main/java/me/domirusz24/pkmagicspells/PKSpellsListener.java
+++ src/main/java/me/domirusz24/pkmagicspells/PKSpellsListener.java
@@ -3,13 +3,10 @@ package me.domirusz24.pkmagicspells;
 import com.projectkorra.projectkorra.BendingPlayer;
 import com.projectkorra.projectkorra.ability.CoreAbility;
 import com.projectkorra.projectkorra.event.BendingPlayerCreationEvent;
-import com.projectkorra.projectkorra.event.PlayerBindChangeEvent;
-import me.domirusz24.pkmagicspells.pk.PKSpellAbility;
 import me.domirusz24.pkmagicspells.pk.SpellBender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemHeldEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PKSpellsListener implements Listener {
     @EventHandler
